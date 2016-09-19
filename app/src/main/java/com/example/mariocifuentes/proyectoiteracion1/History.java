b44f7b80dc6ff098d8da41577d5d977dd6363285
Mackplace <ma.cifuentes51@uniandes.edu.co>
2016-09-18 22:57:40 -0500

    Manda SMS un string que dice prueba envío

--- app/src/main/java/com/example/mariocifuentes/proyectoiteracion1/History.java
+++ app/src/main/java/com/example/mariocifuentes/proyectoiteracion1/History.java
@@ -1,15 +1,32 @@
 package com.example.mariocifuentes.proyectoiteracion1;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class History extends AppCompatActivity {
+    private final static int ESCOGER_CONTACTO = 1;
+    private Button btnContactos;
+    private String nombreContacto;
+    private String numeroTelefonico;
+    private FloatingActionButton btnEnviarAContacto;
+    private String SMS;
+
+
     private static HeartRateDB mydb;
     private static ListView list;
     @Override
@@ -19,11 +36,25 @@ public class History extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+
+
+        btnContactos  = (Button) findViewById(R.id.button3);
+        btnContactos.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                startActivityForResult(new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI), ESCOGER_CONTACTO);
+            }
+        });
+
+        String strReporte = "Prueba Envío";
+        SMS = strReporte;
+
+        btnEnviarAContacto = (FloatingActionButton) findViewById(R.id.fab);
+        btnEnviarAContacto.setOnClickListener(new View.OnClickListener()  {
+            @Override
+            public void onClick(View view) {
+                enviarSMS();
+                Snackbar.make(view, "Tu mensaje fue enviado exitosamente", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
@@ -33,5 +64,53 @@ public class History extends AppCompatActivity {
         ArrayAdapter adap = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mydb.getAllRates());
         list.setAdapter(adap);
     }
+
+    /**
+     * Envía un mensaje de texto al contacto seleccionado
+     */
+    public void enviarSMS() {
+        SmsManager smsManager = SmsManager.getDefault();
+        smsManager.sendTextMessage(numeroTelefonico,null,SMS,null,null);
+    }
+
+    public void mostrarSeleccionContactos(View v) {
+        startActivityForResult(new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI),ESCOGER_CONTACTO);
+        //startActivityForResult(new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.LAST_TIME_CONTACTED),ESCOGER_CONTACTO);
+
+    }
+    /**
+     * Espera el resultado de la selección de contacto
+     */
+    public void onActivityResult(int reqCode, int resultCode, Intent data) {
+        nombreContacto = "";
+        if(resultCode == RESULT_OK){
+            if(reqCode == ESCOGER_CONTACTO){
+                Uri uriContacto = data.getData();
+                if(uriContacto != null ){
+                    try {
+                        String[] cols = {ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME};
+                        Cursor cursor =  managedQuery(uriContacto, cols, null, null, null);
+                        cursor.moveToFirst();
+                        nombreContacto = cursor.getString(0);
+
+                        Uri phoneUri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
+                        String[] columnas = {ContactsContract.CommonDataKinds.Phone.NUMBER};
+                        String seleccion = ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME + "='" + nombreContacto + "'";
+                        Cursor c = managedQuery(phoneUri,columnas,seleccion,null, null );
+                        if(c.moveToFirst()){
+                            numeroTelefonico = c.getString(0);
+                        }
+
+                    } catch (Exception e) {
+                        numeroTelefonico = e.getMessage();
+                     }
+                    btnContactos.setText(nombreContacto + "(Cambiar)");
+                }
+            }
+        }
+
+    }
+
+
 
 }
